cf98504f8b96449810d00dd813ec7ee5394ed190
mrgaren3 <121166488+mrgaren3@users.noreply.github.com>
2025-06-03 14:52:23 +0300

    Add files via upload

--- acceptgame.py
+++ acceptgame.py
@@ -5,7 +5,6 @@ CONFIDENCE_LEVEL = 0.75
 SEARCH_INTERVAL = 0.5
 f = open('used.txt','a+')
 def TryEnterGame():
-    print('trying Enter Game')
     arr = ['findMatch.png', 'accept.png','playAgain.png','continue.png','party.png','confirm.png','change.png']
     accept_button_location=None
     for photo in arr:
@@ -17,11 +16,13 @@ def TryEnterGame():
                 accept_button_center = pyautogui.center(accept_button_location)
                 pyautogui.click(accept_button_center)
         except Exception as e:
-            time.sleep(0.08)
+            time.sleep(0.09)
 
 def CheckenterGame(CONFIDENCE_LEVEL=0.6):
     accept_button_location = None
-    arr2 = ['goingTop.png','goingTop2.png','goingTop3.png','goingDown3.png','goingDown1.png','goingDown6.png','goingDown7.png','goingDown8.png']
+    arr1 = [f'goingTop{i}.png' for i in range(1,5)]
+    arr3 = [f'goingDown{i}.png' for i in range(1,8)]
+    arr2 = arr1+arr3
     for photo in arr2:
         try:
             accept_button_location = pyscreeze.locateOnScreen(
@@ -32,8 +33,18 @@ def CheckenterGame(CONFIDENCE_LEVEL=0.6):
                 f.write(f"{photo}\n")
                 return photo
         except Exception as e:
-            print("not in game")
             time.sleep(0.08)
+
+def isAfk(CONFIDENCE_LEVEL=0.65):
+    accept_button_location = None
+    try:
+        accept_button_location = pyscreeze.locateOnScreen(
+            f"images/IN.png", confidence=CONFIDENCE_LEVEL)
+        if accept_button_location:
+            print('used')
+            return True
+    except Exception as e:
+        time.sleep(0.08)
 
 def CheckWinGame():
     accept_button_location = None
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+
 from functions import *
 from acceptgame import *
 
+counter = 0
+conf = 0.65
+
 while True:
     goingdownPhoto = [f'goingDown{i}.png' for i in range(1,9)]
-    inGame = CheckenterGame()
-    if inGame:
-        if inGame in goingdownPhoto :
-            print("goingDown")
+    inGame = CheckenterGame(conf)
+    isAf0k = isAfk()
+    if inGame or counter>=2:
+        if not inGame:
             X,Y = goingDown()
+            print('warning unknown')
         else:
-
-            
-            print("GoingUP")
-            X,Y = goingTop()
+            if inGame in goingdownPhoto :
+                print("goingDown")
+                X,Y = goingDown()
+            else:
+                print("GoingUP")
+                X,Y = goingTop()
         pucheitem()
         lockScreen()
         Playing(X,Y)
         inGame = None
+        counter = 0
+        conf =0.6
+    elif isAf0k:
+        lockScreen()
+        time.sleep(0.5)
+        lockScreen()
+        conf = 0.55
+        counter+=1
     else:
         TryEnterGame()
 
